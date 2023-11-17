60a8c842233ea9664a3ded61ceb97af2bac04bcf
Dan Imhoff <dwieeb@gmail.com>
2023-11-17 15:22:13 -0500

    Turn off arbitrary value rule

--- recommended.js
+++ recommended.js
@@ -84,7 +84,6 @@ module.exports = {
     'tailwindcss/enforces-negative-arbitrary-values': 'error',
     'tailwindcss/enforces-shorthand': 'error',
     'tailwindcss/migration-from-tailwind-2': 'error',
-    'tailwindcss/no-arbitrary-value': 'error',
     'tailwindcss/no-contradicting-classname': 'error',
     'tailwindcss/no-custom-classname': 'error',
   },
